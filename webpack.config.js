10d0da88101802c0d0bf7dd5e5df45e96f3d13ed
uralberto <urievalo@hotmail.com>
2022-06-06 17:24:12 -0500

    Borrado de imagenes, ajuste webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
   },
   mode: "development",
@@ -66,7 +66,7 @@ module.exports = {
     }),
   ],
   devServer: {
-    static: path.join(__dirname, "dist"),
+    // static: path.join(__dirname, "dist"),
     port: 8080, //agregado
     historyApiFallback: true,
     hot: true, //Agregado
